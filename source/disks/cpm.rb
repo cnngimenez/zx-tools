35fbb1850577349e93edfd18b4a0f88ff5858177
cnngimenez <cnngimenez@disroot.org>
2024-05-21 21:04:36 -0300

    Reading Directory and entries from data.

--- source/disks/cpm.rb
+++ source/disks/cpm.rb
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# frozen_string_literal: true
+
 module CPM
   class Directory
     def initialize
@@ -29,7 +31,32 @@ module CPM
 
     def to_bin
       @entries.map(&:to_bin).join
-    end    
+    end
+
+    private
+
+    # Create a list of 32 byte-string elements from data.
+    def split_data(data)
+      lst = []
+      i = 0
+      while i < data.size
+        lst.push data[i, 32]
+        i += 32
+      end
+      lst
+    end
+
+    class << self
+      def from_bin(data)
+        dir = Directory.new
+
+        split_data(data).map do |entry_data|
+          dir.add Entry.from_bin(entry_data)
+        end
+
+        dir
+      end
+    end
   end
 
   class Entry
@@ -51,9 +78,8 @@ module CPM
     # The total bytes is calculated as: last_bytes * 126
     attr_accessor :last_bytes
 
-    attr_accessor :status, :filename, :extension, :pointers
-
-    attr_accessor :read_only, :system_file, :archived
+    attr_accessor :status, :filename, :extension, :pointers,
+                  :read_only, :system_file, :archived
 
     def ext_to_bin
       ext = @extension.ljust(3).bytes
@@ -63,11 +89,62 @@ module CPM
       ext.pack('CCC')
     end
 
+    # Assign filename and file properties from the given string.
+    #
+    # The file properties for filename is ignored. Therefore, the highest
+    # bit is removed for each byte in this version.
+    #
+    # @todo Implement the filename properties (the high bit).
+    #
+    # Also, remove trailing right spaces.
+    #
+    # @param filename [String] The filename, but the highest bit in each byte
+    #                          has the file property.
+    def filename_with_props=(filename)
+      @filename = filename.bytes.map do |c|
+        b = c & 0b0111_1111
+        b.chr
+      end
+      @filename = @filename.join.rstrip
+    end
+
+    def extension_with_props=(ext)
+      ext2 = ext.bytes
+      @extension = ext2.map do |c|
+        b = c & 0b0111_1111
+        b.chr
+      end
+
+      @read_only = !(ext2[0] & 0b1000_0000).nonzero?.nil?
+      @system_file = !(ext2[1] & 0b1000_0000).nonzero?.nil?
+      @archived = !(ext2[2] & 0b1000_0000).nonzero?.nil?
+
+      @extension = @extension.join.rstrip
+    end
+
     def to_bin
       lstp = @pointers.fill 0, @pointers.length, (16 - @pointers.length)
       lst = [@status, @filename.ljust(8), ext_to_bin, 0, 0, 0, @last_bytes].concat(lstp)
       lst.pack('CA8A3CCCCCCCCCCCCCCCCCCCC')
     end
-  end
 
+    def to_s
+      "#<CPM::Entry #{@filename}.#{@extension}>"
+    end
+
+    class << self
+      def from_bin(data)
+        status, filename, extension, last_bytes = data[0x0, 0x10].unpack 'CA8A3xxxC'
+        pointers = data[0x10, 0x10].unpack 'CCCCCCCCCCCCCCCC'
+        i = pointers.rindex(&:nonzero?)
+        pointers = pointers[0, i + 1]
+
+        e = Entry.new '', '', last_bytes, pointers
+        e.filename_with_props = filename
+        e.extension_with_props = extension
+        e.status = status
+        e
+      end
+    end
+  end
 end
--- tests/test_cpm.rb
+++ tests/test_cpm.rb
@@ -15,37 +15,65 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require 'cpm'
 
-include CPM
-
 class TestCPM < Minitest::Test
+  include CPM
+
   # def setup
   # end
 
   def test_entry_ext_to_bin
-    e = Entry.new  ''
+    e = Entry.new ''
     e.read_only = true
     e.system_file = true
     e.archived = true
 
     assert_equal [160, 160, 160], e.ext_to_bin.bytes
   end
-  
+
   def test_entry_to_bin
     dir = File.binread 'tests/data/manual.dsk', 0x20, 0x1500
     e = Entry.new 'CLOCK', 'BAS', 0x10, [0x02, 0x03]
-    
+
     assert_equal dir.bytes, e.to_bin.bytes
   end
-  
+
+  def test_entry_from_bin
+    dir = File.binread 'tests/data/manual.dsk', 0x20, 0x1500
+    e = Entry.from_bin dir
+
+    assert_equal 0, e.status
+    assert_equal 'CLOCK', e.filename
+    assert_equal 'BAS', e.extension
+    assert_equal 0x10, e.last_bytes
+    assert_equal [0x02, 0x03], e.pointers
+    assert_equal false, e.read_only
+    assert_equal false, e.system_file
+    assert_equal false, e.archived
+  end
+
   def test_directory_to_bin
     dir = File.binread 'tests/data/manual.dsk', 0x20, 0x1500
     mydir = Directory.new
     e = Entry.new 'CLOCK', 'BAS', 0x10, [0x02, 0x03]
     mydir.add e
-    
+
+    assert_equal dir.bytes, mydir.to_bin.bytes
+  end
+
+  def test_directory_to_bin2
+    dir = File.binread 'tests/data/test.dsk', 0x80, 0x1500
+    mydir = Directory.new
+    mydir.add Entry.new 'HOLA', '', 0x02, [0x02]
+    mydir.add Entry.new 'SINUS', 'BAK', 0x03, [0x03]
+    mydir.add Entry.new 'CAPTURE', 'SCR', 0x37,
+                        [0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a]
+    mydir.add Entry.new 'SINUS', '', 0x03, [0x0b]
+
     assert_equal dir.bytes, mydir.to_bin.bytes
   end
 end
